1b8b16f70683818357158f65b082ac382f0bfb65
bkorbuly <b.korbuly@gmail.com>
2017-09-11 13:47:35 +0200

    exercise_01_done

---
+++ Week_02/Day_01/exercise_01/exercise_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Balazs!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
