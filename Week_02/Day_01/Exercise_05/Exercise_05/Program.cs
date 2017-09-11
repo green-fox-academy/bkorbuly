73c53b8001ae5ebc51615b82cca37534927da9a9
bkorbuly <b.korbuly@gmail.com>
2017-09-11 14:28:13 +0200

    exercise_05 done

---
+++ Week_02/Day_01/Exercise_05/Exercise_05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(13 + 22);
+            Console.WriteLine(13 - 22);
+            Console.WriteLine(22 * 13);
+            Console.WriteLine(22 / 13);
+            Console.WriteLine(22 % 13);
+            Console.WriteLine(22 / 13.0);
+
+            Console.ReadLine();
+        }
+    }
+}
