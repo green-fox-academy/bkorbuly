adc4b7cfd9a9dfe5390630ce3f05830d6fdec0d8
bkorbuly <b.korbuly@gmail.com>
2017-09-11 16:15:44 +0200

    exercise 11 done

---
+++ Week_02/Day_01/Exercise_11/Exercise_11/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            a += 10;
+            Console.WriteLine(a);
+
+            int b = 100;
+            b -= 7;
+            Console.WriteLine(b);
+
+            int c = 12;
+            Console.WriteLine(c++);
+            Console.WriteLine(c);
+
+            int d = 12;
+            Console.WriteLine(++d);
+            Console.WriteLine(d);
+
+            int e = 12;
+            Console.WriteLine(e--);
+            Console.WriteLine(e);
+
+            int f = 12;
+            Console.WriteLine(--f);
+            Console.WriteLine(f);
+
+            int g = 12;
+            g *= 3;
+            Console.WriteLine(g);
+
+            int h = 12;
+            h /= 3;
+            Console.WriteLine(h);
+
+            int i = 12;
+            i %= 7;
+            Console.WriteLine(i);
+
+
+            Console.ReadLine();
+
+        
+        }
+    }
+}
