cf80643ea87d00c5bc432767c82eaa2270d14f2d
bkorbuly <b.korbuly@gmail.com>
2017-09-11 15:30:58 +0200

    exercise_09 done

---
+++ Week_02/Day_01/Exercise_08/Exercise_08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+            int c = 0;
+
+            Console.WriteLine("Original order: " + "\n a:" + a + "\n b:" + b );
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("Swapped order:\n a:" + a + "\n b:" + b );
+
+            Console.ReadLine();
+        }
+    }
+}
