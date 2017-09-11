1609e8e5ecb8030bbc8372b02dca303464350eab
bkorbuly <b.korbuly@gmail.com>
2017-09-11 17:57:55 +0200

    exercise 13 done

---
+++ Week_02/Day_01/Exercise_13/Exercise_13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            Console.WriteLine((23*60*60 - currentHours * 60 * 60) + (59 * 60 - currentMinutes * 60) + (60 - currentSeconds));
+            Console.WriteLine(23 * 60 * 60 - currentHours * 60 * 60);
+            Console.WriteLine(59 * 60 - currentMinutes * 60);
+            Console.WriteLine(60 - currentSeconds);
+
+            Console.ReadLine();
+        }
+    }
+}
