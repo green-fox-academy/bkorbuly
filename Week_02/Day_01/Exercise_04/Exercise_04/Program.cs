2bd96c9519d7a53867844dd82620f4b3696cb9d4
bkorbuly <b.korbuly@gmail.com>
2017-09-11 14:11:29 +0200

    exercise_04 done

---
+++ Week_02/Day_01/Exercise_04/Exercise_04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peter Parker");
+            Console.WriteLine("18");
+            Console.WriteLine("1.74");
+
+            Console.ReadLine();
+
+        }
+    }
+}
