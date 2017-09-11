65024992256b7850ac3bf54449d251f81e34d4df
bkorbuly <b.korbuly@gmail.com>
2017-09-11 14:05:09 +0200

    exercise_03 done

---
+++ Week_02/Day_01/Exercise_03/Exercise_03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Lori");
+            Console.WriteLine("Hello, Beci");
+            Console.WriteLine("Hello, Vili");
+
+            Console.ReadLine();
+            }
+    }
+}
