bca291bf5db20eb44b2857ac8d258296bab8a00d
bkorbuly <b.korbuly@gmail.com>
2017-09-11 14:47:25 +0200

    exercise_06 done

---
+++ Week_02/Day_01/Exercise_06/Exercise_06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((6 * (17 * 5)));
+            Console.WriteLine(((6 * (17 * 5)) / (17.0 * 52) * 100) + "%");
+            
+            Console.ReadLine();
+
+        }
+    }
+}
