f05af87aa30da950b11b5140adbe1023711f1d96
bkorbuly <b.korbuly@gmail.com>
2017-09-11 14:56:26 +0200

    exercise_07 done

---
+++ Week_02/Day_01/Exercise_07/Exercise_07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favnum = 34;
+
+            Console.WriteLine("My favorite number is: " + favnum);
+
+            Console.ReadLine();
+        }
+    }
+}
