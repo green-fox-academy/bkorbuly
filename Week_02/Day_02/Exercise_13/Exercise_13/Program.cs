945a1123e04eb950dd2e8de1dfe380fd9f47cd7f
bkorbuly <b.korbuly@gmail.com>
2017-09-12 17:35:07 +0200

    exercise_13 done

---
+++ Week_02/Day_02/Exercise_13/Exercise_13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix= new int[4,4];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if( i == j)
+                    {
+                        matrix[i, j] = 1;
+                        Console.Write(matrix[i,j]);
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                        Console.Write(matrix[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
