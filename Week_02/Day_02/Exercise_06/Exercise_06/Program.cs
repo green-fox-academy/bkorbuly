b2271ad0c91f6591cf862afa4462fdb3b06115a4
bkorbuly <b.korbuly@gmail.com>
2017-09-12 16:37:59 +0200

    exercise_06 done

---
+++ Week_02/Day_02/Exercise_06/Exercise_06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Printer("first", "second", "third", "fourth");
+            Console.ReadLine();
+
+
+        }
+        static public void Printer(params string[] words)
+        {
+            foreach(string word in words)
+            {
+                Console.Write(word + " ");
+            }
+            
+        }
+    }
+}
