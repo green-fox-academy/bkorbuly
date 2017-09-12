ef753efade3c358e97fa0bcd248ebf9827333437
bkorbuly <b.korbuly@gmail.com>
2017-09-12 16:10:29 +0200

    exercise_05 done

---
+++ Week_02/Day_02/Exercise_05/Exercise_05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 5;
+            Console.WriteLine(Factorio(fact));
+            Console.ReadLine();
+        }
+        static public int Factorio(int fact)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= fact; i++)
+            {
+                factorial *= i;                
+            }
+            return factorial;
+        }
+    }
+}
