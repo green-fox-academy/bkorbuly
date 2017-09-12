dd116e2ca51821b5b83e776535828aefe180087d
bkorbuly <b.korbuly@gmail.com>
2017-09-12 13:50:28 +0200

    exercise_04 done

---
+++ Week_02/Day_02/Exercise_04/Exercise_04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            num1 = num2 = num3 = 30;
+            Sum(num1, num2, num3);
+            Console.ReadLine();
+
+        }
+        static public void Sum(int num1, int num2, int  num3)
+        {
+            Console.WriteLine(num1 + num2 + num3);
+        }
+    }
+}
