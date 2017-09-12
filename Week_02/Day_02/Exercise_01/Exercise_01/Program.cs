7d5c6611bba41bed73d7f97271bfc389efcef816
bkorbuly <b.korbuly@gmail.com>
2017-09-12 13:16:38 +0200

    exercise 01 done

---
+++ Week_02/Day_02/Exercise_01/Exercise_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            Console.WriteLine(Doubling(ak));
+            Console.ReadLine();
+        }
+        public static int Doubling(int num1)
+        {
+            num1 *= 2;
+        }
+
+        
+       
+    }
+}
