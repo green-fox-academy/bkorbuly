74345767c59851fcd16dde9bf30420debff51372
bkorbuly <b.korbuly@gmail.com>
2017-09-12 13:41:14 +0200

    exercise_03 done

---
+++ Week_02/Day_02/Exercise_03/Exercise_03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+            AppendA(am);
+            Console.ReadLine();
+        }
+        static void AppendA(string a)
+        {
+            Console.WriteLine(a + "a");
+        }
+    }
+}
