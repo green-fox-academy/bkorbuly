50519edf1aed315f98cd34469cf1dfacb25e0978
bkorbuly <b.korbuly@gmail.com>
2017-09-12 17:05:34 +0200

    exercise_12 done

---
+++ Week_02/Day_02/Exercise_12/Exercise_12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = { 4, 5, 6, 7 };
+            foreach (int num in af)
+                Console.WriteLine(num);
+            Console.ReadLine();
+        }
+    }
+}
