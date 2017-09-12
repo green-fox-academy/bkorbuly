c4831a5fff85dda90c45dd0800198b977ec76322
bkorbuly <b.korbuly@gmail.com>
2017-09-12 16:53:47 +0200

    exercise_09 done

---
+++ Week_02/Day_02/Exercise_09/Exercise_09/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r = { 54, 23, 66, 13 };
+            Console.WriteLine(r[2] + r[3]);
+            Console.ReadLine();
+        }
+    }
+}
