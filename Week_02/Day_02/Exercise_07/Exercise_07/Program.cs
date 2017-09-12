c2e969ec0f97f0294732a7fc9bab2c3457c26279
bkorbuly <b.korbuly@gmail.com>
2017-09-12 16:46:17 +0200

    exercise_07 done

---
+++ Week_02/Day_02/Exercise_07/Exercise_07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] q = { 4, 5, 6, 7 };
+            Console.WriteLine(q[2]);
+            Console.ReadLine();
+        }
+    }
+}
