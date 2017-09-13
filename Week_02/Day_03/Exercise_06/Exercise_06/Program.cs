d57a416d8e3a1f859657d512ddf15c234a1070d4
bkorbuly <b.korbuly@gmail.com>
2017-09-13 16:56:17 +0200

    exercise_06 done

---
+++ Week_02/Day_03/Exercise_06/Exercise_06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+            planetList.Add("Saturn");
+            foreach (string planet in planetList)
+            {
+            Console.WriteLine(planet);
+            }
+            Console.ReadLine();
+        }
+    }
+}
