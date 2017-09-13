250ac86f73ac8b96cee0b578555c1b3a3452476a
bkorbuly <b.korbuly@gmail.com>
2017-09-13 15:06:18 +0200

    exercise_04 done

---
+++ Week_02/Day_03/Exercise_04/Exercise_04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            todoText = todoText.Insert(0, "My todo:\n");
+            todoText = todoText.Insert(todoText.Length, " - Download games");
+            todoText = todoText.Insert(todoText.Length, "\n  - Diablo");
+
+            Console.WriteLine(todoText);
+
+            Console.ReadLine();
+        }
+    }
+}
