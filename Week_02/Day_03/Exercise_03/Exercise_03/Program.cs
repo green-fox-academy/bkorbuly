c2ef2617a385eda7da69618f2662e3746c68c190
bkorbuly <b.korbuly@gmail.com>
2017-09-13 14:53:57 +0200

    exercise_03 done

---
+++ Week_02/Day_03/Exercise_03/Exercise_03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_03
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string strtarget1 = "It you";
+                       
+            StringBuilder quote = new StringBuilder("Hofstadter's Law: It you expect," +
+                "\neven when you take into account Hofstadter's Law.");
+
+            
+            if (quote.ToString().IndexOf(strtarget1) >-1)
+
+            quote.Insert(quote.ToString().IndexOf(strtarget1) +3, "always takes longer than ");
+
+            Console.WriteLine(quote);
+
+           Console.Read();
+        }
+    }
+}
