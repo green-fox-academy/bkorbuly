bbb688d9f1a238a8120626f74985cf41d62c64d2
bkorbuly <b.korbuly@gmail.com>
2017-09-13 16:13:37 +0200

    exercise_05 done

---
+++ Week_02/Day_03/Exercise_05/Exercise_05/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht\n " +
+                ",dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            //char[] array = reversed.ToCharArray();
+            //Array.Reverse(array);
+            //reversed =new string(array);
+
+            //Console.WriteLine(reversed);
+            //Console.ReadLine();        
+            Console.WriteLine(Rev(reversed));
+            Console.ReadLine();
+
+        }
+        static public string Rev(string s)
+        {
+            char[] array = s.ToCharArray();
+            char container;
+            //Console.WriteLine(array.GetLength(0));
+            for (int i = 0; i < (array.GetLength(0)/2); i++)
+            {
+                int endindex = array.GetLength(0) -1 -i;
+                container = array[i];
+                array[i] = array[endindex];
+                array[endindex] = container;
+                //Console.WriteLine("{0} {1}" , i , endindex);
+            }
+            return (new string(array));
+        }
+
+
+    }
+}
