53ae1fd4623204cee1223d4d4979e6104d5d1304
bkorbuly <b.korbuly@gmail.com>
2017-09-13 13:09:43 +0200

    exercise_02 done

---
+++ Week_02/Day_03/Example_02/Example_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            string strtofind = "https";
+
+            url = url.Replace("bots", "odds");
+
+            Console.WriteLine(url.IndexOf(strtofind));
+
+            url = url.Insert(url.IndexOf(strtofind) + 5, ":");
+
+            Console.WriteLine(url);
+
+            Console.ReadLine();
+        }
+    }
+}
