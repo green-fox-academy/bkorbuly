86823b6ce096ff3de49d87fd07e218c0a1a67cdb
bkorbuly <b.korbuly@gmail.com>
2017-09-14 10:14:32 +0200

    exercise_14 done

---
+++ Week_02/Day_01/Exercise_14/Exercise_14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello,World!\nPlease give me your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            Console.ReadLine();
+
+        }
+    }
+}
