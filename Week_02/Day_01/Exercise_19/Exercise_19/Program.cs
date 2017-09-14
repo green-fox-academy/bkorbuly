da98d4ffa80efe39019b44ef0dd977b4062a2f03
bkorbuly <b.korbuly@gmail.com>
2017-09-14 13:22:22 +0200

    exercise_19 done

---
+++ Week_02/Day_01/Exercise_19/Exercise_19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num <= 0)
+                {
+                    Console.WriteLine("Not enough");
+                }
+                else if (num == 1)
+                {
+                    Console.WriteLine("One");
+                }
+                else if (num == 2)
+                {
+                    Console.WriteLine("Two");
+                }
+                else
+                {
+                    Console.WriteLine("A lot");
+                }
+            Console.ReadLine();
+        }
+    }
+}
