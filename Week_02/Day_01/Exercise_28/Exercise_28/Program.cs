8ce9cfe915d83aab425d8b997e1378365e60ee43
bkorbuly <b.korbuly@gmail.com>
2017-09-14 16:41:43 +0200

    exercise_28 done

---
+++ Week_02/Day_01/Exercise_28/Exercise_28/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int line;
+            line = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < line; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j <=i; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
