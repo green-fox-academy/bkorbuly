a2a33c3c8cb3fb81fcd5c9745e8bf35176adf53d
bkorbuly <b.korbuly@gmail.com>
2017-09-14 10:38:46 +0200

    exercise_15 done

---
+++ Week_02/Day_01/Exercise_15/Exercise_15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int km;
+
+            Console.Write("Please add the km: ");
+
+            km = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("It equals " + km * 0.621371192 + " miles");
+
+            Console.ReadLine();
+
+        }
+    }
+}
