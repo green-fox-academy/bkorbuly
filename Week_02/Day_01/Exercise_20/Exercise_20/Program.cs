2e8e2faa77773f9f2572a8b566608f8cfc60d0ba
bkorbuly <b.korbuly@gmail.com>
2017-09-14 13:41:39 +0200

    exercise_20 done

---
+++ Week_02/Day_01/Exercise_20/Exercise_20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] read = new string[2];
+            read = Console.ReadLine().Split(' ');
+            if (Convert.ToInt32(read[0]) > Convert.ToInt32(read[1]))
+            {
+                Console.WriteLine(read[0]);
+            }
+            else
+            {
+                Console.WriteLine(read[1]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
