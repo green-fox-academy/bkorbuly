a74bdd61808f1ce881f6c4401e57d688eae3a87b
bkorbuly <b.korbuly@gmail.com>
2017-09-14 12:04:10 +0200

    exercise_18 done

---
+++ Week_02/Day_01/Exercise_18/Exercise_18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string read;
+            int num;
+            read = Console.ReadLine();
+            num = Convert.ToInt32(read);
+            if ( (num % 2) == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
