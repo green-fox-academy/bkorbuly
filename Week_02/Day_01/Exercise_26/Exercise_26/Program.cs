3ab36bc324239b97f144d5929f21bda04e4e5727
bkorbuly <b.korbuly@gmail.com>
2017-09-14 16:28:34 +0200

    exercise_26 done

---
+++ Week_02/Day_01/Exercise_26/Exercise_26/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("first number {0}, second number {1}, counting:\n", num1, num2 );
+            if (num1 >= num2)
+            {
+                Console.WriteLine("The second number should be bigger");
+            }
+            else
+            {
+                for (int i=num1; i < num2; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+                    
+
+            Console.ReadLine();
+        }
+    }
+}
