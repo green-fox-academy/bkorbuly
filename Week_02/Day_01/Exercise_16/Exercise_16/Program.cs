699692a22469bdad917a91dcbe6401a66f95d5ad
bkorbuly <b.korbuly@gmail.com>
2017-09-14 10:49:32 +0200

    exercise_16 done

---
+++ Week_02/Day_01/Exercise_16/Exercise_16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int chicken, pig;
+
+            Console.Write("Please add how many chicken(s) is/are on the farm: ");
+            chicken = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(chicken);
+
+            Console.Write("Please add how many pig(s) is/are on the farm: ");
+            pig = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("There are " + (chicken * 2 + pig * 4) + " total legs are on the farm");
+
+            Console.ReadLine();
+        }
+    }
+}
