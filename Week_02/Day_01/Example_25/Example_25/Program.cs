0db3dc0f13dc8e026bd53f571185a14f7ad957c6
bkorbuly <b.korbuly@gmail.com>
2017-09-14 16:21:03 +0200

    exercise_25 done

---
+++ Week_02/Day_01/Example_25/Example_25/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine("{0} * {1} = " + (i*num),i ,num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
