52bebf839d1ab5dea624c085baa26ded7becc5b0
bkorbuly <b.korbuly@gmail.com>
2017-09-15 13:16:25 +0200

    Palindrome_project_Done

---
+++ Week_02/Day_05/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PalindromeFunction());
+            Console.ReadLine();
+
+        }
+
+        private static string PalindromeFunction()
+        {
+            string word;
+            char[] array; 
+            word = Console.ReadLine();
+            array = word.ToCharArray();
+            for (int i = array.Length - 1 ; i >= 0; i--)
+            {
+                word = word + array[i];
+            }
+            return word;
+        }
+    }
+}
