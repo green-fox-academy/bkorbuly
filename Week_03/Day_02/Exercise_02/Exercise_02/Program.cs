e2cb6b876802941b8a60aae30ba214c1e9dc2ba6
bkorbuly <b.korbuly@gmail.com>
2017-09-19 13:33:54 +0200

    exercise_02 done with Streamreader method

---
+++ Week_03/Day_02/Exercise_02/Exercise_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercise_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            try
+            {
+                using (StreamReader reader = new StreamReader("C:/Users/Balazs/Greenfox/Week_03/Day_02/my_file.txt"))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("{0}", e);
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
