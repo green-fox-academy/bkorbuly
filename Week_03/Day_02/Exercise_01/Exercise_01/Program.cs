0e35978d6f48aa2479de338535a6f3103d2e5475
bkorbuly <b.korbuly@gmail.com>
2017-09-19 11:02:52 +0200

    Divided function added

---
+++ Week_03/Day_02/Exercise_01/Exercise_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divide();
+            Console.ReadLine();
+        }
+
+        private static void Divide()
+        {
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num);
+        }
+    }
+}
