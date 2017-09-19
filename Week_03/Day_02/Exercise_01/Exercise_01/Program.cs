43300422b40249e6b6e0519bcd63a8f5c5202a0b
bkorbuly <b.korbuly@gmail.com>
2017-09-19 11:39:10 +0200

    DividebyZero_feature added

--- Week_03/Day_02/Exercise_01/Exercise_01/Program.cs
+++ Week_03/Day_02/Exercise_01/Exercise_01/Program.cs
@@ -10,15 +10,46 @@ namespace Exercise_01
     {
         static void Main(string[] args)
         {
-            Divide();
+            DivideZerobyTen();
+            DivideNumbyZero();
             Console.ReadLine();
         }
 
-        private static void Divide()
+        private static void DivideNumbyZero()
         {
-            int num;
-            num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(num);
+            int num, result;
+            num = int.Parse(Console.ReadLine());
+            try
+            {
+                result = 10 / num;
+                Console.WriteLine(num);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Fail");
+            }
+        } 
+            
+            
+        private static void DivideZerobyTen()
+        {
+            int num = 0;
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine()) / 10;
+                if (num == 0)
+                {
+                    throw new ArithmeticException();
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Not possible to divide 0 by any number");
+            }
         }
     }
 }
