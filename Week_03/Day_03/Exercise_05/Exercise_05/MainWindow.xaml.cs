91c34d86f841d8086bb6fc5ed544966f7a91aef1
bkorbuly <b.korbuly@gmail.com>
2017-09-20 15:24:20 +0200

    Exercise_05 done and enhanced with random coordinates

--- Week_03/Day_03/Exercise_05/Exercise_05/MainWindow.xaml.cs
+++ Week_03/Day_03/Exercise_05/Exercise_05/MainWindow.xaml.cs
@@ -12,17 +12,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GreenFox;
 
 namespace Exercise_05
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            Random rnd = new Random();
+            int coordinate1 = rnd.Next(0, 251);
+            int coordinate2 = rnd.Next(0, 301);
+            var pairstart = new Point(12 , 12);
+            var pairstart2 = new Point(coordinate1, coordinate2);
+            var pairstart3 = new Point(rnd.Next(0 , 251) , rnd.Next(0 , 301 ));
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.BackgroundColor(Colors.Yellow);
+            LineDraw(foxDraw , pairstart);
+            LineDraw(foxDraw, pairstart2);
+            LineDraw(foxDraw, pairstart3);
         }
+
+        private void LineDraw(FoxDraw canvas, Point pairstart)
+        {
+           var pairend = new Point(pairstart.X + 50 , pairstart.Y);
+           canvas.DrawLine(pairstart, pairend);
+        }
+
     }
 }
