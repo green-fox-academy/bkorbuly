c0454b3aa1213a75bef62e42bdb368e6c83edc8e
bkorbuly <b.korbuly@gmail.com>
2017-09-20 11:24:45 +0200

    Exercise_01 done

---
+++ Week_03/Day_03/Exercise_01/Exercise_01/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Exercise_01
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.BackgroundColor(Colors.Aqua);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, 150, 300, 150);
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(150, 0, 150, 300);
+        }
+    }
+}
