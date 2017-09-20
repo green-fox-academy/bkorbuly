8b05a21d20fb5d46296c7c13c9670c69b0e268f7
bkorbuly <b.korbuly@gmail.com>
2017-09-20 15:49:51 +0200

    Suqare function ready to use

---
+++ Week_03/Day_03/Exercise_08/Exercise_08/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Exercise_08
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            var startpoint = new Point(30 , 30);
+            DrawSquare(foxDraw, startpoint);
+        }
+
+        private void DrawSquare(FoxDraw foxDraw, Point startpoint)
+        {
+            var pointlist = new List<Point>()
+            {
+                new Point (startpoint.X, startpoint.Y),
+                new Point (startpoint.X + 50 , startpoint.Y),
+                new Point (startpoint.X + 50 , startpoint.Y + 50),
+                new Point (startpoint.X , startpoint.Y + 50)
+            };
+            foxDraw.DrawPolygon(pointlist);
+        }
+    }
+}
