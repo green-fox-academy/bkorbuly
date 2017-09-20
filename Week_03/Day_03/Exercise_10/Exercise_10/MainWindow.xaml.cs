f6131c5320f823cd76b513b9a546aaa5c2534dd4
bkorbuly <b.korbuly@gmail.com>
2017-09-20 16:53:00 +0200

    Exercise_10 done

--- Week_03/Day_03/Exercise_10/Exercise_10/MainWindow.xaml.cs
+++ Week_03/Day_03/Exercise_10/Exercise_10/MainWindow.xaml.cs
@@ -17,12 +17,37 @@ using GreenFox;
 namespace Exercise_10
 {
     
+
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
+            var middlepoint = new Point(150, 150);
+            int size = 150;
+            for(int i = 0; i < 30; i++)
+            {
+                size = size - 5;
+                    //rnd.Next(0 , 20);
+                DrawSquare(foxDraw, middlepoint, size);
+            }
+            
+        }
+
+        private void DrawSquare(FoxDraw foxDraw, Point middlepoint, int size)
+        {
+            var squarepoints = new List<Point>()
+            {
+                new Point(middlepoint.X - size , middlepoint.Y - size ),
+                new Point(middlepoint.X + size , middlepoint.Y - size ),
+                new Point(middlepoint.X + size , middlepoint.Y + size ),
+                new Point(middlepoint.X - size , middlepoint.Y + size ),
+            };
+            foxDraw.FillColor(Color.FromArgb(255, Convert.ToByte(rnd.Next(0, 256)), Convert.ToByte(rnd.Next(0, 256)), Convert.ToByte(rnd.Next(0, 256))));
+            foxDraw.DrawPolygon(squarepoints);
         }
     }
 }
