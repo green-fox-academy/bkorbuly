c9d944629ec6d9b1498496821432dd584ed51c42
bkorbuly <b.korbuly@gmail.com>
2017-09-20 11:53:37 +0200

    Exercise_02 updated

--- Week_03/Day_03/Exercise_02/Exercise_02/MainWindow.xaml.cs
+++ Week_03/Day_03/Exercise_02/Exercise_02/MainWindow.xaml.cs
@@ -27,12 +27,20 @@ namespace Exercise_02
             var pointList = new List<Point>()
             {
                 new Point(10, 10),
-                new Point(50, 10),
-                new Point(50, 50),
-                new Point(10, 50),
+                new Point(290, 10),
+                new Point(290, 290),
+                new Point(10, 290),
             };
-            foxDraw.FillColor(Colors.Black);
-            foxDraw.DrawPolygon(pointList);
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.DrawLine(pointList[0],pointList[1]);
+            foxDraw.StrokeColor(Colors.Yellow);
+            foxDraw.DrawLine(pointList[1], pointList[2]);
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(pointList[2], pointList[3]);
+            foxDraw.StrokeColor(Colors.Purple);
+            foxDraw.DrawLine(pointList[3], pointList[0]);
+            //foxDraw.FillColor(Colors.Black);
+            //foxDraw.DrawPolygon(pointList);
         }
     }
 }
