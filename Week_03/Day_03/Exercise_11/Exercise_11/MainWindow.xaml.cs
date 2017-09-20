2171e3f039da7ec6fc690b618c17357983420347
bkorbuly <b.korbuly@gmail.com>
2017-09-20 17:26:00 +0200

    Exercise_11 done

---
+++ Week_03/Day_03/Exercise_11/Exercise_11/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Exercise_11
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            Point start = new Point(10, 10);
+            int size = 20;
+            for (int i = 0; i < 14; i++)
+            {
+                DrawSquares(foxDraw, size, start);
+                start.X = start.X + size;
+                start.Y = start.Y + size;
+            } 
+            
+
+        }
+
+        private void DrawSquares(FoxDraw foxDraw, int size, Point start)
+        {
+            var pointlist = new List<Point>()
+            {
+                new Point(start.X , start.Y),
+                new Point(start.X + size , start.Y),
+                new Point(start.X + size , start.Y + size),
+                new Point(start.X , start.Y + size),
+            };
+            foxDraw.FillColor(Colors.Purple);
+            foxDraw.DrawPolygon(pointlist);
+        }
+    }
+}
