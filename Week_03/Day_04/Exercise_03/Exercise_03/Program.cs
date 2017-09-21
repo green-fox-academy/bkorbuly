b357a3300f573a325709018ed9e9cd928d6c799d
bkorbuly <b.korbuly@gmail.com>
2017-09-21 11:56:17 +0200

    Exercise_03 done

---
+++ Week_03/Day_04/Exercise_03/Exercise_03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(DigitSum(num));
+            Console.ReadLine();
+        }
+
+        private static int DigitSum(int num)
+        {
+            if (num / 10 > 0)
+            {
+                return (num % 10) + DigitSum(num / 10);
+            }
+            else
+            {
+                return num;
+            }
+        }
+    }
+}
