80ba030e9ee5606db7459807be06a56693ecdf8f
bkorbuly <b.korbuly@gmail.com>
2017-09-21 17:30:45 +0200

    Exercise_08 done

---
+++ Week_03/Day_04/Exercise_08/Exercise_08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            word = Console.ReadLine();
+            Console.WriteLine(RemoveXFromString(word));
+            Console.ReadLine();
+        }
+
+        private static string RemoveXFromString(string word)
+        {
+            if ( word.Contains("x"))
+            {
+                return RemoveXFromString(word.Remove(word.IndexOf("x"), 1));                
+            }
+            return word;
+        }
+    }
+}
