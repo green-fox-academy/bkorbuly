4ce216658604026f667ac89b3981b0af863aced8
bkorbuly <b.korbuly@gmail.com>
2017-09-21 13:44:07 +0200

    Excerise_05

---
+++ Week_03/Day_04/Exercise_05/Exercise_05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bunniesnumber;
+            bunniesnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(CountEars(bunniesnumber));
+            Console.ReadLine();
+        }
+
+        private static int CountEars(int bunniesnumber)
+        {
+            if ( bunniesnumber > 0)
+            {
+                return  2 + CountEars(--bunniesnumber); 
+            }
+            return bunniesnumber;
+        }
+    }
+}
