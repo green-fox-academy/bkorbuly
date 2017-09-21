f6dc9a57e497b871886587ae4b0925cd7da33174
bkorbuly <b.korbuly@gmail.com>
2017-09-21 16:00:25 +0200

    Exercise_06 done

---
+++ Week_03/Day_04/Exercise_06/Exercise_06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bunnies = 0;
+            bunnies = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(CountEars(bunnies));
+            Console.ReadLine();
+        }
+
+        private static int CountEars(int bunnies)
+        {
+            if (bunnies > 0)
+            {
+                if ( bunnies % 2 == 1)
+                {
+                    Console.WriteLine(bunnies + " 3 ears");
+                    return 3 + CountEars(--bunnies);
+                }
+                Console.WriteLine(bunnies + " 2 ears");
+                return 2 + CountEars(--bunnies);
+            }
+            return bunnies;
+        }
+    }
+}
