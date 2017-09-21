eeecffb7302c4cc8e5cf0977aa6c616d20fbcf86
bkorbuly <b.korbuly@gmail.com>
2017-09-21 10:22:14 +0200

    Exercise_01 done

---
+++ Week_03/Day_04/Exercise_01/Exercise_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            Counter(num);
+            Console.ReadLine();
+        }
+        public static int Counter(int num)
+        {
+            if(num == 0 )
+            {
+                Console.WriteLine("0");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(num);
+                return Counter(num - 1);
+            }
+        }
+    }
+}
