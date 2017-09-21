304ea78ef217386836047351b6832828c13ba0b1
bkorbuly <b.korbuly@gmail.com>
2017-09-21 12:05:36 +0200

    Exercise_04 done

---
+++ Week_03/Day_04/Exercise_06/Exercise_06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int basen, powerto;
+            basen = powerto = 0;
+            basen = Convert.ToInt32(Console.ReadLine());
+            powerto = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PowerToN(basen, powerto));
+            Console.ReadLine();
+        }
+
+        public static int PowerToN(int basen, int powerto)
+        {
+            if ( powerto > 1 )
+            {
+                return basen * PowerToN(basen, --powerto);
+            }
+
+            return basen;
+            
+        }
+    }
+}
