4bf2c567fa428dece69c7a2aab67c5c3b62dbc03
bkorbuly <b.korbuly@gmail.com>
2017-09-25 15:33:47 +0200

    Exercise_02 done

---
+++ Week_04/Day_01/Exercise_02_BlogPost/Exercise_02_BlogPost/BlogPost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_02_BlogPost
+{
+    public class BlogIt
+        {
+            string AuthorName;
+            string Title;
+            string Text;
+            string PublicationDate;
+
+            public BlogIt(string Authorname, string Title, string Text, string PublicationDate)
+            {
+                this.AuthorName = Authorname;
+                this.Title = Title;
+                this.Text = Text;
+                this.PublicationDate = PublicationDate;
+
+            Console.WriteLine("{1}\n{0}\n{2}\n{3}\n\n", Authorname, Title, Text, PublicationDate);
+            }
+        }
+}
---
+++ Week_04/Day_01/Exercise_02_BlogPost/Exercise_02_BlogPost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_02_BlogPost
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlogIt blog1 = new BlogIt("John Doe", "Lorensum", "Lorem ipsum dolor sit amet.", "2000.05.04.");
+            BlogIt blog2 = new BlogIt("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
+            BlogIt blog3 = new BlogIt("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "A popular long-form, stick-figure-illustrated blog about almost everything.Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28.");
+            Console.ReadLine();
+        }
+    }
+}
