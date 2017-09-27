ebe45cf065bc89a3c3b61b67b07ad508ba39a44c
bkorbuly <b.korbuly@gmail.com>
2017-09-27 17:52:37 +0200

    Exercise_04 function added

---
+++ Week_04/Day_03/Exercise_04_CountLetters/Exercise_04_CountLetters/CountLetters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_04_CountLetters
+{
+    public class CountLetters
+    {
+        public Dictionary<char, int> CountLetter(string text1)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            char[] array = text1.ToCharArray();
+            Array.Sort(array);
+            for(int i = 0; i < array.Length; i++)
+            {
+                int counter = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if(array[i] == array[j])
+                    {
+                        counter++;
+                    }
+                }
+                if (!map.ContainsKey(array[i]))
+                {
+                    map.Add(array[i], counter);
+                }                
+            }
+            return map;
+        }
+    }
+}
---
+++ Week_04/Day_03/Exercise_04_CountLetters/Exercise_04_CountLetters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_04_CountLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inst = new CountLetters();
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            map = inst.CountLetter("ablak ami nagyon ablak");
+            foreach (var element in map)
+            {
+                Console.WriteLine("{0}, {1}",  element.Key, element.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
