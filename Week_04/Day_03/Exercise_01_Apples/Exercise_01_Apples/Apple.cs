10a64f46460d6055753d92849c78f99db7175347
bkorbuly <b.korbuly@gmail.com>
2017-09-27 11:22:33 +0200

    Apple class done

---
+++ Week_04/Day_03/Exercise_01_Apples/Exercise_01_Apples/Apple.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01_Apples
+{
+    class Apple
+    {
+        private string message = "apple";
+
+        public string GetApple()
+        {
+            return message;
+        }
+    }
+}
