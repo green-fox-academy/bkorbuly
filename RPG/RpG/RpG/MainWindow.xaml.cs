9e88aafadce0c07ec5fdf7acf944c0d174bf6794
bkorbuly <b.korbuly@gmail.com>
2017-10-03 14:50:09 +0200

    Map base object added, Tile child object added

---
+++ RPG/RpG/RpG/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace RpG
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Map.Canvas = canvas;
+            Tile tile = new Tile();
+            tile.DrawTiles();            
+        }
+        private void WindowKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                Console.WriteLine("To the left!");
+            }
+
+            if (e.Key == Key.Right)
+            {
+                Console.WriteLine("To the right!");
+            }
+        }
+    }
+}
---
+++ RPG/RpG/RpG/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace RpG
+{
+    public class Tile : Map
+    {
+        public void DrawTiles()
+        {
+            string floor = @"C:\Users\Balazs\Greenfox\RPG\RpG\RpG\Asset\floor.png";
+            for(int i = 0; i < 10; i++)
+            {
+                //foxdraw.BackgroundColor(Colors.AliceBlue);
+                Point p1 = new Point(12, 123);
+                Point p2 = new Point(150, 123);
+                foxdraw.DrawLine(p1, p2);
+                foxdraw.AddImage(floor, 10, 10);
+            }
+            
+        }
+    }
+}
