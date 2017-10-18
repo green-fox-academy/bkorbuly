7ec79b3feb88cb7e7398dfecdf1228565866a7c9
bkorbuly <b.korbuly@gmail.com>
2017-10-18 15:28:36 +0200

    Exercise_05 Done

---
+++ Week_06/Day_03/Exercise_05/Exercise_05/Exercise_05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            Console.WriteLine("Query:");
+            QueryFrequency(n);
+            Console.WriteLine("\nLambda:");
+            LambdaFrequency(n);
+            Console.ReadLine();
+        }
+
+        private static void QueryFrequency(int[] n)
+        {
+            var qFrequency = from num in n
+                             group num by num into x
+                             orderby x.Key
+                             select x;
+            foreach(var element in qFrequency)
+            {
+                Console.WriteLine("{0} {1}", element.Key, element.Count());
+            }
+        }
+
+        private static void LambdaFrequency(int[] n)
+        {
+            var lFrequency = n.OrderBy(x => x).GroupBy(x => x);
+            foreach (var element in lFrequency)
+            {
+                Console.WriteLine("{0} {1}", element.Key, element.Count());
+            }
+        }
+    }
+}
