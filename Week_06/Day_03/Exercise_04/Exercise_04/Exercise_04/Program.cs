9cc9e4c01ab833481869da970fa2d1bf0f5b9a2f
bkorbuly <b.korbuly@gmail.com>
2017-10-18 14:07:32 +0200

    Exercise_03 done with Query and Lambda solutions

---
+++ Week_06/Day_03/Exercise_04/Exercise_04/Exercise_04/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+            Console.WriteLine("Query:");
+            QueryGreaterThanTwenty(n);
+            Console.WriteLine("\nLambda:");
+            LambdaGreaterThanTwenty(n);
+            Console.ReadLine();
+        }
+
+       
+
+        private static void QueryGreaterThanTwenty(int[] n)
+        {
+            var sqaureGreaterThanTwenty = 
+                from number in n
+                where number * number > 20
+                select number;
+            foreach(int num in sqaureGreaterThanTwenty)
+            {
+                Console.Write(num + " ");
+            }
+        }
+        private static void LambdaGreaterThanTwenty(int[] n)
+        {
+            var sqaureGreaterThanTwenty = n.Where(x => (x * x) > 20);
+            foreach(int num in sqaureGreaterThanTwenty)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
