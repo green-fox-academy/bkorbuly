6af0c2824386fe6c99add82b93d2ababd4a1182d
bkorbuly <b.korbuly@gmail.com>
2017-10-18 11:51:20 +0200

    Exercise_02 done by both Query and Lamda

---
+++ Week_06/Day_03/Exercise_02/Exercise_02/Exercise_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            Console.WriteLine("Query:");
+            Console.WriteLine(program.QueryAverage(n));
+            Console.WriteLine("Lamda:");
+            Console.WriteLine(program.LamdaAverage(n));
+            Console.ReadLine();
+        }
+        public double QueryAverage(int[] n)
+        {
+            return (from numbers in n select numbers).Average();
+        }
+        public double LamdaAverage(int[] n)
+        {
+            return n.Average(x => x);
+        }
+    }
+}
