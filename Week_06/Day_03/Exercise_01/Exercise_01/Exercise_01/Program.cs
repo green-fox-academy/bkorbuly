71b7b699787952de670139708715a158bdd7e28c
bkorbuly <b.korbuly@gmail.com>
2017-10-18 11:16:54 +0200

    Excerise_01 done by both Query and Lamda solutions.

---
+++ Week_06/Day_03/Exercise_01/Exercise_01/Exercise_01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var program = new Program();
+            Console.WriteLine("Lamda:");
+            program.LamdaEven(n);
+            Console.WriteLine("\nQuery:");
+            QueryEven(n);
+            Console.ReadLine();
+        }
+        public static void QueryEven(int[] n)
+        {
+            var evenNumbers =
+               from num in n
+               where num % 2 == 0
+               select num;
+            foreach (var evenNumber in evenNumbers)
+            {
+                Console.Write(evenNumber + " ");
+            }
+        }
+        public void LamdaEven(int[] n)
+        {
+            var evenNumbers = n.Where(num => num % 2 == 0); 
+            foreach (var evenNumber in evenNumbers)
+            {
+                Console.Write(evenNumber + " ");
+            }
+        }
+    }
+}
