4a3ccfc4bf71b46c2c0f31454c1e833e4592186e
bkorbuly <b.korbuly@gmail.com>
2017-10-18 16:13:34 +0200

    Exercise_07 done

---
+++ Week_06/Day_03/Exercise_07/Exercise_07/Exercise_07/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            Console.WriteLine("Query:");
+            QueryCities(cities);
+            Console.WriteLine("Lambda:");
+            LambdaCities(cities);
+            Console.ReadLine();
+        }
+
+        private static void QueryCities(string[] cities)
+        {
+            var cityAI = from city in cities
+                         where city.IndexOf('A') == 0 && city.Last() == 'I'
+                         select city;
+            foreach(var city in cityAI)
+            {
+                Console.WriteLine(city);
+            }
+        }
+
+        private static void LambdaCities(string[] cities)
+        {
+            var cityAI = cities.Where(x => x.IndexOf('A') == 0 && x.Last() == 'I');
+            foreach (var city in cityAI)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
