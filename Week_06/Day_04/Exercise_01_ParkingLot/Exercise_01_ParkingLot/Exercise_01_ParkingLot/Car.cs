4b24deaac6987cb4bbd620f55109cd0980af4960
bkorbuly <b.korbuly@gmail.com>
2017-10-19 11:51:31 +0200

    Car class done, Car list filled by using enum

---
+++ Week_06/Day_04/Exercise_01_ParkingLot/Exercise_01_ParkingLot/Exercise_01_ParkingLot/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01_ParkingLot
+{
+    class Car
+    {
+        static Random rnd = new Random();
+        public static List<Car> carlist = new List<Car>();
+       
+
+        public Car(CarType type, CarColor color)
+        {
+            
+        }
+        public enum CarType
+        {
+            sedan = 0,
+            coupe = 1,
+            sport = 2,
+            family = 3,
+            hatchback = 4,
+            SUV= 5
+        }
+        public enum CarColor
+        {
+            yellow = 0,
+            green = 1,
+            black = 2,
+            blue = 3,
+            red = 4,
+            grey = 5,
+            purple = 6,
+            pink = 7
+        }
+        public static List<Car> CarInitialization()
+        {
+            
+            for(int i = 0; i < 257; i++)
+            {
+                CarType type = (CarType)rnd.Next(0, 6);
+                CarColor color = (CarColor)rnd.Next(0, 8);
+                carlist.Add(new Car(type, color));
+                Console.WriteLine("{0} {1}", type, color);
+            }
+
+            return carlist;
+        }
+    }
+}
---
+++ Week_06/Day_04/Exercise_01_ParkingLot/Exercise_01_ParkingLot/Exercise_01_ParkingLot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01_ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car.CarInitialization();
+            Console.ReadLine();
+        }
+    }
+}
