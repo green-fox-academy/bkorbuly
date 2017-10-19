5c2c06bdb1039e4e9ed382c5bb3631846e53dd60
bkorbuly <b.korbuly@gmail.com>
2017-10-19 16:14:02 +0200

    Card class with enums created

---
+++ Week_06/Day_04/Exercise_02_TwentyPlusOne/Exercise_02_TwentyPlusOne/Exercise_02_TwentyPlusOne/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_02_TwentyPlusOne
+{
+    class Card
+    {
+        public enum Color
+        {
+            red,
+            black
+        }
+        public enum Suit
+        {
+            clubs,
+            diamonds,
+            hearts,
+            spades
+        }
+        public enum Rank
+        {
+           two = 2,
+           three = 3,
+           four = 4,
+           five = 5,
+           six = 6,
+           seven = 7,
+           eight = 8,
+           nine = 9,
+           ten = 10,
+           J,
+           Q,
+           K,
+           A
+        }
+    }
+}
