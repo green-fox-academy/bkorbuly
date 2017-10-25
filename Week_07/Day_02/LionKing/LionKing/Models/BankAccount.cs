0871bbd60b8717346a6702539efef937ee3aca96
bkorbuly <b.korbuly@gmail.com>
2017-10-25 11:48:19 +0200

    MVC added, bankaccount class added

---
+++ Week_07/Day_02/LionKing/LionKing/Models/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LionKing.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Animaltype { get; set; }
+    }
+}
