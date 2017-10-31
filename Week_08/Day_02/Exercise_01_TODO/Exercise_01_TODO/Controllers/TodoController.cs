b71bf8562c6d3df39e3ff801d4c31faa3c975ecb
bkorbuly <b.korbuly@gmail.com>
2017-10-31 14:03:00 +0100

    exercise_01 initaited

---
+++ Week_08/Day_02/Exercise_01_TODO/Exercise_01_TODO/Controllers/TodoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Exercise_01_TODO
+{
+    public class TodoController : Controller
+    {
+        [Route("/todo")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [Route("/")]
+        [Route("/list")]
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
