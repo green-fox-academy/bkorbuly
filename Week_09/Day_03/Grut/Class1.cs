968d95cf5eec3bc86e5816dc28d2a4eb0e61fd7f
bkorbuly <b.korbuly@gmail.com>
2017-11-08 16:21:31 +0100

    Grut

---
+++ Week_09/Day_03/Grut/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Grut
+{
+    public class Class1
+    {
+    }
+}
---
+++ Week_09/Day_03/Grut/Grut/Controllers/GuardianController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Groot.Controllers
+{
+    public class GuardianController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/groot")]
+    }
+}
