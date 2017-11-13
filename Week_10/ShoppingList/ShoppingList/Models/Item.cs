60e41841e06c40c3b7c8b7f28f951ba8858e031a
bkorbuly <b.korbuly@gmail.com>
2017-11-13 13:07:50 +0100

    Item and User classes added

---
+++ Week_10/ShoppingList/ShoppingList/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    public class Item
+    {
+        public long Id { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+    }
+}
